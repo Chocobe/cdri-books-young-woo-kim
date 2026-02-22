a8b366b2ed01632fe32e8e85f6e23b870b31f4d6
Miles Kim <kyw05171@gmail.com>
2026-02-23 03:25:23 +0900

    create ChevronDownIcon

---
+++ common/assets/svgIcons/ChevronDownIcon.tsx
@@ -0,0 +1,11 @@
+import { SVGProps } from 'react';
+
+function ChevronDownIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M4.0001 4.7998L8.0001 8.7998L12.0001 4.7998L13.6001 5.5998L8.0001 11.1998L2.4001 5.5998L4.0001 4.7998Z" fill="currentColor"/>
+    </svg>
+  );
+}
+
+export default ChevronDownIcon;
