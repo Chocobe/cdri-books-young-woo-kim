272269deee3d0c4040e1a2fe0d1921d203e77017
Miles Kim <kyw05171@gmail.com>
2026-02-25 01:09:15 +0900

    apply `Noto Sans KR` font to default font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { Noto_Sans_KR } from 'next/font/google';
 import QueryClientProviderImpl from '@/common/utils/react-query/QueryClientProviderImpl';
+import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const notoSansKR = Noto_Sans_KR({
+  variable: '--font-noto-sans-kr',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${notoSansKR.variable}`}
       >
         <div id="root">
           <QueryClientProviderImpl>
